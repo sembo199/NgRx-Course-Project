bd5c9ae0a3c8d6d838eca1c2a82de12c86c3d25f
Sem Ekkelboom <S.Ekkelboom@sharevalue.nl>
2021-12-01 16:11:07 +0100

    8-implementing-the-login-screen

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 import {Store} from "@ngrx/store";
@@ -7,6 +7,7 @@ import {AuthService} from "../auth.service";
 import {tap} from "rxjs/operators";
 import {noop} from "rxjs";
 import {Router} from "@angular/router";
+import { AppState } from '../../reducers';
 
 @Component({
   selector: 'login',
@@ -20,7 +21,8 @@ export class LoginComponent implements OnInit {
   constructor(
       private fb:FormBuilder,
       private auth: AuthService,
-      private router:Router) {
+      private router:Router,
+      private store: Store<AppState>) {
 
       this.form = fb.group({
           email: ['test@angular-university.io', [Validators.required]],
@@ -35,6 +37,20 @@ export class LoginComponent implements OnInit {
 
   login() {
 
+    const val = this.form.value;
+
+    this.auth.login(val.email, val.password)
+      .pipe(
+        tap(user => {
+          this.store.
+
+          this.router.navigateByUrl('/courses');
+        })
+      )
+      .subscribe(
+        noop,
+        () => alert('Login Failed')
+      );
   }
 
 }
