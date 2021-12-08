6caf92902cb95ae46253b4bcd49bda737a978f35
Sem Ekkelboom <S.Ekkelboom@sharevalue.nl>
2021-12-08 15:41:09 +0100

    26-NgRx-feature-design

---
+++ src/app/courses/course.actions.ts
@@ -0,0 +1,4 @@
+import { createAction } from "@ngrx/store";
+
+export const loadAllCourses = createAction('[Courses Resolver] Load All Courses');
+export const allCoursesLoaded = createAction('[Load Courses Effect] All Courses Loaded');
