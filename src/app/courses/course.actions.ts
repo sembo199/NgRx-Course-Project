b40d624968f9e0ffc7c2ca9683a79def32128a5d
Sem Ekkelboom <S.Ekkelboom@sharevalue.nl>
2021-12-15 15:07:18 +0100

    34-optimistically-editing-entity-data

--- src/app/courses/course.actions.ts
+++ src/app/courses/course.actions.ts
@@ -1,3 +1,4 @@
+import { Update } from "@ngrx/entity";
 import { createAction, props } from "@ngrx/store";
 import { Course } from "./model/course";
 
@@ -5,4 +6,8 @@ export const loadAllCourses = createAction('[Courses Resolver] Load All Courses'
 export const allCoursesLoaded = createAction(
   '[Load Courses Effect] All Courses Loaded',
   props<{courses: Course[]}>()
+);
+export const courseUpdated = createAction(
+  "[Edit Course Dialog] Course Updated",
+  props<{update: Update<Course>}>()
 );
